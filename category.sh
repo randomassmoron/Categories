d1a5bbcd8bf401407dfec62cb7e8ba5d3abc2765
randomassmoron <115660188+randomassmoron@users.noreply.github.com>
2023-02-15 19:28:24 +0200

    Update and rename script.sh to category.sh

--- category.sh
+++ category.sh
@@ -13,35 +13,30 @@ sed -n '/U3Ry1/,/U3Ry2/{/U3Ry1/!{/U3Ry2/!p;};}' $filename
 
 categoria2() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry3/,/U3Ry4/{/U3Ry3/!{/U3Ry4/!p;};}' $filename
 
 }
 
 categoria3() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry5/,/U3Ry6/{/U3Ry5/!{/U3Ry6/!p;};}' $filename
 
 }
 
 categoria4() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry7/,/U3Ry8/{/U3Ry7/!{/U3Ry8/!p;};}' $filename
 
 }
 
 categoria5() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry9/,/U3Ry10/{/U3Ry9/!{/U3Ry10/!p;};}' $filename
 
 }
 
 categoria6() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry11/,/U3Ry12/{/U3Ry11/!{/U3Ry12/!p;};}' $filename
 
 }
@@ -49,14 +44,12 @@ sed -n '/U3Ry11/,/U3Ry12/{/U3Ry11/!{/U3Ry12/!p;};}' $filename
 
 categoria7() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry13/,/U3Ry14/{/U3Ry13/!{/U3Ry14/!p;};}' $filename
 }
 
 
 categoria8() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry15/,/U3Ry16/{/U3Ry15/!{/U3Ry16/!p;};}' $filename
 }
 
@@ -64,7 +57,7 @@ sed -n '/U3Ry15/,/U3Ry16/{/U3Ry15/!{/U3Ry16/!p;};}' $filename
 
 categoria9() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
+
 sed -n '/U3Ry17/,/U3Ry18/{/U3Ry17/!{/U3Ry18/!p;};}' $filename
 
 
@@ -72,7 +65,6 @@ sed -n '/U3Ry17/,/U3Ry18/{/U3Ry17/!{/U3Ry18/!p;};}' $filename
 
 categoria10() {
 
-        #ia stringul dintre U3Ry1 si U3Ry2 din fisier
 sed -n '/U3Ry19/,/U3Ry20/{/U3Ry19/!{/U3Ry20/!p;};}' $filename
 
 }
@@ -82,7 +74,7 @@ sed -n '/U3Ry19/,/U3Ry20/{/U3Ry19/!{/U3Ry20/!p;};}' $filename
 
 
 
-#code lol
+#code
 echo "do you want to see the strings for categories
 1) yes
 2) no
@@ -201,10 +193,6 @@ fi
 
 
 
-
-
-
-#else statement -------
 	else
 	echo "categories.txt doesn't exist"
 
@@ -217,25 +205,3 @@ echo "the file doesn't exist"
 fi
 
 
-
-
-#comments -------------------
-
-
-#cat la un file
-
-#grep daca are un string in fisier si acel string sa fie o categorie
-
-<<comment
-case pt a gasi nr cateogiei
-cand gaseste acea categorie, o variabila devine 1
-apoi da cat la tot pana la urmatoarea categorie
-cand vine urmatoarea categorie aia devine 0 si se termina
-
-	make categories.txt
-
-o sa fie numelestring aia si o sa fie incrementata cu 1 daca nu e gasita categoria pana o gaseste
-cand gaseste o sa dea print la text inafara de variabila
-cand se termina categoria se termina loopul (gen cand c3Rn apare la sfarsitul categoriei, se opreste)
-
-comment
